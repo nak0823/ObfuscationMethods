b72521b63d9257bd4b5a51271a076aa73fecb780
Nakateru <130221150+nak0823@users.noreply.github.com>
2023-04-16 13:09:50 +0200

    Anti Protections

---
+++ Anti/Anti Dnspy/AntiDnspy.cs
@@ -0,0 +1,27 @@
+﻿using dnlib.DotNet;
+using dnlib.DotNet.Emit;
+
+namespace Protector.Protections
+{
+    internal class AntiDnspy
+    {
+        public static void Execute(Context ctx)
+        {
+            // This code implements an obfuscation technique called "AntiDnspy" that adds a large number of NOP (no operation) instructions to the beginning of each method's body in the target assembly.
+            // This makes it more difficult for tools like dnSpy to decompile the code, as the extra NOP instructions will need to be removed before the decompiled code can be properly analyzed.
+            // The code loops through all the types and methods in the target assembly and adds 33333 NOP instructions to the start of each method's body.
+
+            foreach (TypeDef type in ctx.assemblyDef.ManifestModule.Types)
+            {
+                foreach (MethodDef method in type.Methods)
+                {
+                    if (method.Body == null) continue;
+                    for (int x = 0; x < 33333; x++)
+                    {
+                        method.Body.Instructions.Insert(x, new Instruction(OpCodes.Nop));
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Anti/Anti Ildasm/AntiIldams.cs
@@ -0,0 +1,21 @@
+﻿using dnlib.DotNet;
+
+namespace Satfuscator.Protections
+{
+    internal class AntiIldasm
+    {
+        public static void Execute(Context ctx)
+        {
+            // The code is an implementation of the AntiIldasm protection, which prevents disassembly of .NET assemblies using tools like Ildasm. This specific method is called when the protection is executed and it iterates over each module in the assembly to add a custom attribute that marks the module as SuppressIldasm
+
+            foreach (ModuleDefMD module in ctx.assemblyDef.Modules)
+            {
+                TypeRef attrRef = module.CorLibTypes.GetTypeRef("System.Runtime.CompilerServices", "SuppressIldasmAttribute");
+                var ctorRef = new MemberRefUser(module, ".ctor", MethodSig.CreateInstance(module.CorLibTypes.Void), attrRef);
+
+                var attr = new CustomAttribute(ctorRef);
+                module.CustomAttributes.Add(attr);
+            }
+        }
+    }
+}
