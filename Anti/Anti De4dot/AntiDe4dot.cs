6a2a1107a09166cfcbc15f4e5ce75db3e4a34d90
Nakateru <130221150+nak0823@users.noreply.github.com>
2023-04-16 12:49:35 +0200

    Create AntiDe4dot.cs

---
+++ Anti/Anti De4dot/AntiDe4dot.cs
@@ -0,0 +1,26 @@
+﻿using dnlib.DotNet;
+
+namespace Protector.Protections
+{
+    internal class AntiDe4dot
+    {
+        public static void Execute(Context context)
+        {
+            // Adds invalid interface implementations to crash De4dot
+
+            foreach (ModuleDef module in context.assemblyDef.Modules)
+            {
+                InterfaceImplUser invalidImpl = new InterfaceImplUser(module.GlobalType);
+
+                for (int i = 100; i < 150; i++)
+                {
+                    TypeDefUser invalidType = new TypeDefUser("", Renamer.GenerateName(), module.CorLibTypes.GetTypeRef("System", "Attribute"));
+                    InterfaceImplUser typeImpl = new InterfaceImplUser(invalidType);
+                    module.Types.Add(invalidType);
+                    invalidType.Interfaces.Add(typeImpl);
+                    invalidType.Interfaces.Add(invalidImpl);
+                }
+            }
+        }
+    }
+}
